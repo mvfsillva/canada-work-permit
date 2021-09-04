48b92cf7cd4f7ee724a2d807d29ec9399cb8e5f3
Marcus Silva <mvfsillva@gmail.com>
2021-09-04 13:53:45 -0300

    feat: centralize all component export

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Button } from './button';
+export { default as Card } from './card';
+export { default as DateField } from './date-field';
+export { default as Header } from './header';
+export { default as Input } from './input';
+export { default as Select } from './select';
