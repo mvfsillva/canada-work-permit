3f8a141c0122096cd466a74a61dd45cf43cadb58
Marcus Silva <mvfsillva@gmail.com>
2021-09-05 23:45:40 -0300

    deps: update packages

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,18 +1,23 @@
-export type VisaTypes = 'WP' | 'OWP' | 'GTS, OWP' | 'WP, OWP' | 'GTS, OWP, VISITOR'
+export type VisaTypes =
+  | 'WP'
+  | 'OWP'
+  | 'GTS, OWP'
+  | 'WP, OWP'
+  | 'GTS, OWP, VISITOR'
 export type CategoryTypes = 'LMIA' | 'LMIA EXEMPT' | 'GTS'
 export type StatusTypes = 'awaiting' | 'approved' | 'not approved'
 
 export interface ApplicationType {
-  id: string;
-  name?: string;
-  noc: number;
-  application_date: Date;
-  application_year: Date;
-  visa_response_date?: Date;
-  processing_week_time: string;
-  processing_month_time: string;
-  status: StatusTypes;
-  approved?: boolean;
-  visa_type: VisaTypes;
-  category: CategoryTypes;
+  id: string
+  name?: string
+  noc: number
+  application_date: Date
+  application_year: Date
+  visa_response_date?: Date
+  processing_week_time: string
+  processing_month_time: string
+  status: StatusTypes
+  approved?: boolean
+  visa_type: VisaTypes
+  category: CategoryTypes
 }
