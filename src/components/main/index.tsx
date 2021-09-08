ae1b451d18558c70650e75f637d679266a18d6aa
Marcus <mvfsillva@gmail.com>
2021-09-08 03:18:46 -0300

    refactor: change firebase request to hooks

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,85 +1,87 @@
-import { useEffect, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
+import { useListVals as useListValues } from 'react-firebase-hooks/database'
 import useSWR from 'swr'
 import 'twin.macro'
 
-import { Card } from 'components'
+import { firebase } from 'services'
+import { pluralize } from 'helpers'
+
+import { Card, Header } from 'components'
 import { Box } from 'layout'
 import { ApplicantList } from 'containers'
-import { getAll } from 'services'
 import * as S from './styles'
+
+import type { ApplicationType } from 'types'
 
 const fetcher = (url) => fetch(url).then((res) => res.json())
 
 const NEXT_PUBLIC_IRCC_TIME = process.env.NEXT_PUBLIC_IRCC_TIME
 
+const ERROR_MESSAGE = 'An error has occurred'
+
 const Main = () => {
-  const [applications, setApplications] = useState([])
+  const [values, loading, error] = useListValues<ApplicationType>(
+    firebase.ref('/applications')
+  )
   const [time, setTime] = useState<Record<string, number>>({
     shortest: 0,
     longest: 0
   })
-  const { data: ircc, error } = useSWR(NEXT_PUBLIC_IRCC_TIME, fetcher, {
-    onErrorRetry: (error, key, option, revalidate, { retryCount }) => {
-      if (retryCount >= 2) return
-      if (error.status === 404) return
 
-      setTimeout(() => revalidate({ retryCount: retryCount + 1 }), 3000)
-    }
-  })
+  const { data: ircc, error: irccError } = useSWR(
+    NEXT_PUBLIC_IRCC_TIME,
+    fetcher
+  )
 
-  const getApplications = async () => {
-    const data = await getAll()
-    setApplications(data)
-  }
-
-  const getLongestApplication = () => {
-    const result = applications.map((item) => {
-      return item.date_processing_week
-    })
+  const getApplicationTime = useCallback(() => {
+    const result = values
+      .map((item) =>
+        +item.date_processing_week > 0 ? +item.date_processing_week : 0.1
+      )
+      .filter(Boolean) as unknown as number[]
 
     const longest = Math.max(...result)
     const shortest = Math.min(...result)
 
     setTime({ longest, shortest })
-  }
+  }, [values])
 
   useEffect(() => {
-    getApplications()
-    getLongestApplication()
-  }, [])
+    getApplicationTime()
+  }, [getApplicationTime])
 
-  if (!ircc) return <Box>Loading...</Box>
+  if (!ircc || loading) return <Box>Loading...</Box>
+  if (error) return <Box>{ERROR_MESSAGE}</Box>
+
+  console.log(time.shortest)
 
   return (
     <S.Container>
-      <div tw="py-5">
-        <h1 tw="text-lg leading-3 font-medium text-gray-900">
-          Brazilians Work Permit - Process Time
-        </h1>
-        <h2 tw="mt-1 max-w-2xl text-sm text-gray-500">
-          Details and application.
-        </h2>
-      </div>
+      <Header />
       <div tw="flex items-center justify-center mb-4 space-x-4">
         <Card
           title="IRCC"
-          content={error ? 'An error has occurred' : ircc?.work?.BR}
+          content={irccError ? ERROR_MESSAGE : ircc?.work?.BR}
           footer={`Last update: ${ircc?.work?.lastupdated}`}
           variant="gray"
         />
         <Card
           title="Longest waiting"
-          content={`${time.longest} weeks`}
+          content={`${pluralize(time.longest, 'week')}`}
           footer="Based on our applications history"
         />
         <Card
           title="shorter waiting"
-          content={`${time.shortest} weeks`}
+          content={
+            time.shortest > 0.1
+              ? pluralize(time.shortest, 'week')
+              : 'less than 1 week'
+          }
           variant="skyBlue"
           footer="Based on our applications history"
         />
       </div>
-      <ApplicantList applications={applications} />
+      <ApplicantList applications={values} />
     </S.Container>
   )
 }
