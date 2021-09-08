dde2a6f950e6375e40ab430081e2bf4875159abb
Marcus <mvfsillva@gmail.com>
2021-09-08 03:19:34 -0300

    refactor: remove form logic

--- src/containers/applicant-list/index.tsx
+++ src/containers/applicant-list/index.tsx
@@ -1,43 +1,26 @@
 import 'twin.macro'
 import { useState } from 'react'
-import { useForm, FormProvider as ReactFormProvider } from 'react-hook-form'
-import { Fields, Filter } from 'containers'
+import { useRouter } from 'next/router'
+import { useCopyToClipboard } from 'react-use'
+import { Filter } from 'containers'
 import { Button } from 'components'
-import { create } from 'services'
+import { pluralize } from 'helpers'
 
 import type { ApplicationType } from 'types'
 import { TableHead, TableItem, TableItemEditButton } from './table-partials'
-
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const FormProvider = ReactFormProvider as unknown as any
 
 type ApplicantListProps = {
   applications: Partial<ApplicationType[]>
 }
 
 export default function ApplicantList({ applications }: ApplicantListProps) {
-  const [showForm, setShowForm] = useState(false)
-  const [editPerson, setEditPerson] = useState({})
+  const router = useRouter()
+  const [, copyToClipboard] = useCopyToClipboard()
   const [filter, setFilter] = useState({
     category: 'all',
     visaType: 'all',
     status: 'all'
   })
-  const methods = useForm()
-
-  console.log(methods.watch())
-
-  const onSubmit = (data) => {
-    console.log(data)
-    create(data)
-    methods.reset()
-    setShowForm(false)
-  }
-
-  const handleEditApplication = (person: Partial<ApplicationType>) => {
-    setShowForm(true)
-    setEditPerson(person)
-  }
 
   const handleFilter = ({ target }) => {
     const { name, value: item } = target
@@ -47,54 +30,52 @@ export default function ApplicantList({ applications }: ApplicantListProps) {
     })
   }
 
+  let approvedList = ''
+  applications.forEach((item) => {
+    const dateProcessingWeek =
+      +item.date_processing_week > 0
+        ? pluralize(+item.date_processing_week, 'week')
+        : 'less than 1 week'
+    if (item.status === 'approved') {
+      approvedList += `${item.name} | ${item.noc} | ${item.application_date} | ${item.visa_type} | ${item.category} | ${item.visa_response_date} | ${dateProcessingWeek}\n`
+    }
+  })
+
   return (
     <div tw="flex flex-col">
       <div tw="flex items-center mb-4 space-x-4">
-        <Button variant="skyBlue" onClick={() => setShowForm(true)}>
+        <Button variant="skyBlue" onClick={() => router.push('/new-applicant')}>
           New Applicant
         </Button>
-        <Button
-          variant="gray"
-          onClick={() => console.log('Copy approved list')}
-        >
+        <Button variant="gray" onClick={() => copyToClipboard(approvedList)}>
           Copy approved list
         </Button>
       </div>
       <Filter handleFilter={handleFilter} />
-      {!showForm ? (
-        <div tw="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-          <div tw="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-            <div tw="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-              <table tw="min-w-full divide-y divide-gray-200">
-                <TableHead />
-                <tbody tw="bg-white divide-y divide-gray-200">
-                  {applications.map((person, index) => (
-                    <tr key={index}>
-                      <TableItem item={person.name} subItem={person.noc} />
-                      <TableItem item={person.application_date} />
-                      <TableItem item={person.application_year} />
-                      <TableItem item={person.date_processing_week} />
-                      <TableItem status={person.status} />
-                      <TableItem item={person.visa_response_date} />
-                      <TableItemEditButton
-                        handleEditApplication={() =>
-                          handleEditApplication(person)
-                        }
-                      />
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      ) : (
-        <FormProvider {...methods} defaultValues={editPerson}>
-          <form onSubmit={methods.handleSubmit(onSubmit)}>
-            <Fields handleCancel={() => setShowForm(false)} />
-          </form>
-        </FormProvider>
-      )}
+      <div tw="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <table tw="min-w-full divide-y divide-gray-200">
+          <TableHead />
+          <tbody tw="bg-white divide-y divide-gray-200">
+            {applications.map((person, index) => (
+              <tr key={index}>
+                <TableItem item={person.name} subItem={person.noc} />
+                <TableItem item={person.application_date} />
+                <TableItem item={person.application_year} />
+                <TableItem
+                  item={
+                    +person.date_processing_week > 0
+                      ? pluralize(+person.date_processing_week, 'week')
+                      : 'less than 1 week'
+                  }
+                />
+                <TableItem status={person.status} />
+                <TableItem item={person.visa_response_date} />
+                <TableItemEditButton />
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
