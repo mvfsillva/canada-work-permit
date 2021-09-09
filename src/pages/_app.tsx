a370e234e11e6a60c0a4403dafbf7dedaebb1507
Marcus Silva <mvfsillva@gmail.com>
2021-09-09 19:41:25 -0300

    chore: add icons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,25 @@ const App = ({ Component, pageProps }: AppProps) => (
   <ThemeProvider theme={theme}>
     <I18nextProvider i18n={i18n}>
       <Head>
-        <meta name="theme-color" content="#FBAE16" />
+        <meta name="theme-color" content="#EE5253" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest" />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
