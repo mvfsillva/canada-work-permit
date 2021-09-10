ab6095aea3034cb6bd8b5c73b011f9f8d45c0725
Marcus Silva <mvfsillva@gmail.com>
2021-09-10 02:13:50 -0300

    refactor: change form to dynamic route

--- src/layout/container.ts
+++ src/layout/container.ts
@@ -4,7 +4,7 @@ import media from 'styled-media-query'
 export const Container = styled.div`
   ${({ theme }) => css`
     max-width: ${theme.grid.container};
-    padding: ${theme.spaces.xxsmall} calc(${theme.grid.gutter} / 2);
+    padding: ${theme.spaces.xxxsmall} calc(${theme.grid.gutter} / 2);
     margin: 0 auto;
 
     ${media.greaterThan('medium')`
--- src/pages/applicant/[...domain].tsx
+++ src/pages/applicant/[...domain].tsx
@@ -1,18 +1,25 @@
 import { useForm, FormProvider as ReactFormProvider } from 'react-hook-form'
 import { useRouter } from 'next/router'
 import { toast } from 'react-toastify'
+import { useObjectVal } from 'react-firebase-hooks/database'
 
 import { Header } from 'components'
 import { Container } from 'layout'
 import { Fields } from 'containers'
-import { create } from 'services'
+import { create, firebase } from 'services'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const FormProvider = ReactFormProvider as unknown as any
 
 const NewApplicant = () => {
-  const methods = useForm()
   const router = useRouter()
+  const { domain } = router.query
+  const isEdit = !!domain
+  const id = isEdit ? domain[1] : null
+
+  const [values] = useObjectVal(firebase.ref(`/applications/${id}`))
+
+  const methods = useForm()
   const onSubmit = (data) => {
     toast.success('Applicant added successfully')
     create(data)
@@ -23,7 +30,7 @@ const NewApplicant = () => {
   return (
     <Container>
       <Header />
-      <FormProvider {...methods}>
+      <FormProvider {...methods} defaultValues={values}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
           <Fields />
         </form>
