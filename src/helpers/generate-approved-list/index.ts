945120438b1922d0f6d0397c99bdc89f45039bdd
Marcus <mvfsillva@gmail.com>
2021-09-11 20:19:01 -0300

    feat: create generateApprovedList function

---
+++ src/helpers/generate-approved-list/index.ts
@@ -0,0 +1,38 @@
+import { pluralize } from 'helpers'
+
+function renderDateProcessingWeek(date_processing_week) {
+  return date_processing_week > 0
+    ? pluralize(date_processing_week, 'week')
+    : 'less than 1 week'
+}
+
+const generateApprovedList = (list) => {
+  let shareCompleteList = ''
+  let shareNocList = ''
+
+  list.sort(
+    (a, b) =>
+      +new Date(b.val().visa_response_date) -
+      +new Date(a.val().visa_response_date)
+  )
+
+  list.forEach((item, index) => {
+    const dateProcessingWeek = renderDateProcessingWeek(
+      item.val().date_processing_week
+    )
+
+    if (item.val().status === 'approved') {
+      shareCompleteList += `${item.val().name} | ${item.val().noc} | ${
+        item.val().application_date
+      } | ${item.val().visa_type} | ${item.val().category} | ${
+        item.val().visa_response_date
+      } | ${dateProcessingWeek}\n`
+
+      shareNocList += `${index} ${item.val().name} ${item.val().noc} ✅ \n`
+    }
+  })
+
+  return [shareCompleteList, shareNocList]
+}
+
+export default generateApprovedList
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,2 +1,3 @@
 export { default as convertStringToDate } from './convert-string-to-date'
 export { default as pluralize } from './pluralize'
+export { default as generateApprovedList } from './generate-approved-list'
