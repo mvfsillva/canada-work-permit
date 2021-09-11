496fcf71f117989f05371c7966140ccbafa4ea8a
Marcus <mvfsillva@gmail.com>
2021-09-11 19:24:21 -0300

    feat: add edit methods

--- src/pages/applicant/[...domain].tsx
+++ src/pages/applicant/[...domain].tsx
@@ -1,4 +1,4 @@
-import { useForm, FormProvider as ReactFormProvider } from 'react-hook-form'
+import { useForm, FormProvider } from 'react-hook-form'
 import { useRouter } from 'next/router'
 import { toast } from 'react-toastify'
 import { useObjectVal } from 'react-firebase-hooks/database'
@@ -6,33 +6,39 @@ import { useObjectVal } from 'react-firebase-hooks/database'
 import { Header } from 'components'
 import { Container } from 'layout'
 import { Fields } from 'containers'
-import { create, firebase } from 'services'
+import { firebase, firebaseCreate, firebaseUpdate } from 'services'
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-const FormProvider = ReactFormProvider as unknown as any
+import type { ApplicationType } from 'types'
 
 const NewApplicant = () => {
   const router = useRouter()
-  const { domain } = router.query
-  const isEdit = !!domain
-  const id = isEdit ? domain[1] : null
-
-  const [values] = useObjectVal(firebase.ref(`/applications/${id}`))
-
   const methods = useForm()
+  const { domain } = router.query
+
+  const isEdit = domain && domain[0] === 'edit'
+  const id = isEdit ? domain[1] : null
+  const applicantRef = firebase.ref(`/applications/${id}`)
+
+  const [values] = useObjectVal<ApplicationType>(applicantRef)
+
   const onSubmit = (data) => {
-    toast.success('Applicant added successfully')
-    create(data)
+    toast.success(`Applicant ${isEdit ? 'updated' : 'added'} successfully`)
     methods.reset()
     router.push('/')
+
+    if (isEdit) {
+      return firebaseUpdate(id, data)
+    }
+
+    return firebaseCreate(data)
   }
 
   return (
     <Container>
       <Header />
-      <FormProvider {...methods} defaultValues={values}>
+      <FormProvider {...methods}>
         <form onSubmit={methods.handleSubmit(onSubmit)}>
-          <Fields />
+          <Fields values={isEdit && values} />
         </form>
       </FormProvider>
     </Container>
