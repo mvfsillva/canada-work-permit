a52a9db628cebce09ab09c851eaa23f77d370fa0
Marcus <mvfsillva@gmail.com>
2021-09-11 21:02:29 -0300

    feat: add footer

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,16 @@
+import * as S from './styles'
+
+const Footer = () => {
+  return (
+    <S.Wrapper>
+      <p>
+        Made with <S.Love>♥</S.Love> by
+        <a href="https://mvfsillva.dev" target="blank">
+          Marcus Silva
+        </a>
+      </p>
+    </S.Wrapper>
+  )
+}
+
+export default Footer
---
+++ src/components/footer/styles.ts
@@ -0,0 +1,50 @@
+import styled, { css, keyframes } from 'styled-components'
+import media from 'styled-media-query'
+
+const pulse = keyframes`
+  0% { transform: scale( .75 ) }
+  40% { transform: scale( .75 ) }
+  60% { transform: scale( 1 ) }
+  80% { transform: scale( .75 ) }
+  100% { transform: scale( .75 ) }
+`
+
+export const Wrapper = styled.footer`
+  ${({ theme }) => css`
+    color: ${theme.colors.darkGray};
+    text-align: center;
+    font-size: ${theme.font.sizes.xxxsmall};
+    line-height: ${theme.font.sizes.medium};
+    transition: ${theme.transition.ease};
+    margin-top: ${theme.spaces.small};
+    padding-bottom: ${theme.spaces.small};
+
+    ${media.greaterThan('medium')`
+      font-size: ${theme.font.sizes.xxxsmall};
+      margin-top: ${theme.spaces.xxsmall};
+      padding-bottom: ${theme.spaces.xxsmall};
+    `}
+
+    a {
+      color: ${theme.colors.primary};
+      font-weight: bold;
+      text-decoration: none;
+      margin-left: 0.2rem;
+      :hover {
+        color: ${theme.colors.secondary};
+      }
+    }
+  `}
+`
+
+export const Love = styled.span`
+  ${({ theme }) => css`
+    display: inline-block;
+    position: relative;
+    top: 0.2rem;
+    font-size: ${theme.font.sizes.small};
+    color: ${theme.colors.secondary};
+    transform: scale(0.2);
+    animation: ${pulse} 0.5s linear infinite alternate-reverse;
+  `}
+`
