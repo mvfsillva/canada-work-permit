a443f486eb1c9f4880bba6fe4dca2a0ba2579a9e
Marcus <mvfsillva@gmail.com>
2021-09-12 23:32:07 -0300

    fix: update visa type

--- src/services/firebase-methods.ts
+++ src/services/firebase-methods.ts
@@ -36,8 +36,10 @@ export const firebaseCreate = (person) => {
 
 export const firebaseUpdate = (id, data) => {
   const applicantRef = firebase.ref(`/applications/${id}`)
+  console.log(data.visa_type.value)
   const payload = {
     ...data,
+    visa_type: data.visa_type.value || data.visa_type,
     status:
       data.visa_response_date && data.status === 'awaiting'
         ? 'approved'
