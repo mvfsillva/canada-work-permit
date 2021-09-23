7281e3b7ac7b7bf2b63cf6d52c0e145bef48142a
Marcus Silva <mvfsillva@gmail.com>
2021-09-22 23:14:00 -0300

    style: fix border on chrome browser

--- src/components/input/styles.ts
+++ src/components/input/styles.ts
@@ -10,9 +10,16 @@ export const Field = styled.input<{
     min-height: 36px;
     background-color: ${disabled && theme.colors.muted};
     border: 1px solid ${hasError ? theme.colors.primary : theme.colors.black};
+    border-radius: ${theme.border.radius};
     color: ${theme.colors.black};
     padding: 0.6rem;
     outline-color: transparent;
+
+    :focus,
+    :hover {
+      outline-color: ${theme.colors.darkGray};
+    }
+
     ::placeholder {
       color: ${theme.colors.darkGray};
     }
