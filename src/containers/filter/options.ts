e082eae9022442deb2b75fbee7e3ad1df610eb68
Marcus Silva <mvfsillva@gmail.com>
2022-03-28 13:37:33 -0700

    refactor: update year options

--- src/containers/filter/options.ts
+++ src/containers/filter/options.ts
@@ -20,5 +20,6 @@ export const statusOptions = [
 export const yearOptions = [
   { value: '2019', label: '2019' },
   { value: '2020', label: '2020' },
-  { value: '2021', label: '2021' }
+  { value: '2021', label: '2021' },
+  { value: '2022', label: '2022' }
 ]
