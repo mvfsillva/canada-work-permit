81a3950046ef4467c0c53a4877a5da5332685a93
Victor Lucas <victorlcmonteiro@gmail.com>
2022-06-09 21:20:42 -0300

    feat: authenticate supabase

--- src/services/supabase-config.ts
+++ src/services/supabase-config.ts
@@ -4,3 +4,17 @@ export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
+
+export const authenticate = async () => {
+  try {
+    const { session } = await supabase.auth.signIn({
+      email: process.env.NEXT_PUBLIC_SUPABASE_USER,
+      password: process.env.NEXT_PUBLIC_SUPABASE_PASSWORD
+    })
+
+    return session.access_token
+  } catch (e) {
+    // TODO: send this error to crashlytics or something like that
+    console.error(e)
+  }
+}
