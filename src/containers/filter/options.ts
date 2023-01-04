8d0818b9555dfdee25b437a5ae3ce5441d17e63f
Marcus Silva <mvfsillva@gmail.com>
2023-01-04 10:33:03 -0800

    fix: add new year 2023 (#19)

--- src/containers/filter/options.ts
+++ src/containers/filter/options.ts
@@ -17,9 +17,11 @@ export const statusOptions = [
   { value: 'not approved', label: 'Not approved' }
 ]
 
+// Needs to improve (I'll send some improvements tomorrow)
 export const yearOptions = [
   { value: '2019', label: '2019' },
   { value: '2020', label: '2020' },
   { value: '2021', label: '2021' },
-  { value: '2022', label: '2022' }
+  { value: '2022', label: '2022' },
+  { value: '2023', label: '2023' }
 ]
